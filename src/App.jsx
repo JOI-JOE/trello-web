05c3b60fbd7ce68203b82f8c800cc884a9f17b86
JOI-JOE <iknowhtml161gmail.com>
2025-01-15 12:00:56 +0700

    Update install mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { Button } from "@mui/material";
-
 function App() {
   return (
     <>
       <div>ngodanghau</div>
-      <Button variant="contained">Hello</Button>
-      <Button variant="text">Text</Button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
