aee4519f8df1412fe2832388b32b7540d2d582a3
JOI-JOE <iknowhtml161gmail.com>
2025-01-16 09:29:42 +0700

    Add dark mode part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useColorScheme } from '@mui/material/styles'
+import { Button } from '@mui/material'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+        // localStorage.setItem('trello-dark-ligh-mode')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
   return (
     <>
+      <ModeToggle />
       <div>ngodanghau</div>
     </>
   )
