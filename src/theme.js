72922d4b795204b26009f4cc9ffc7fce86ce07f7
JOI-JOE <iknowhtml161gmail.com>
2025-01-18 20:10:16 +0700

    App bar p2

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,3 @@
-// import { createTheme } from '@mui/material/styles'
 import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
@@ -23,6 +22,57 @@ const theme = extendTheme({
           main: cyan[500],
           secondary: orange[500]
         }
+      }
+    }
+  },
+  components: {
+    // Name of the component
+    MuiCssBaseline:{
+      styleOverrides: {
+        body:{
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#8395a7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#10ac84'
+          }
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main
+        })
+      }
+    },
+    MuiOutlinedInput:{
+      styleOverrides:{
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem',
+          '.MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.light
+          },
+          '&:hover': {
+            '.MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.main
+            }
+          }
+        })
       }
     }
   }
